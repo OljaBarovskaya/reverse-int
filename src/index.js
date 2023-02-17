79eb9faed821f44db1866cb17f6455d9a156beb0
OljaBarovskaya <asdfg1234_08@mail.ru>
2023-02-17 21:40:18 +0300

    reverse-int done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    let arr = n.toString().split('');
+  if (n<0) {
+    arr.shift();
+  }
+  let reverseArr = arr.reverse();
+  let newString = reverseArr.join('');
+  return Number(newString);
+    }
+
